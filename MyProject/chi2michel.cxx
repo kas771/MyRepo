02822ff0e7d4cd2523f7511f5dc350b050e4983d
Kathryn Sutton <kathrynsutton@sandbox084.nevis.columbia.edu>
2015-07-10 16:29:11 -0400

    now really that way

--- MyProject/chi2michel.cxx
+++ MyProject/chi2michel.cxx
@@ -26,7 +26,9 @@ namespace larlite {
     range = 6;
     
     end_plot = new TH2D("end_plot", "", 100, 0, 0, 100, 0, 0);
-    end-plot->SetTitle("Muon Endpoint for Chi-Square Window of 7 Hits");
+    end_plot->SetTitle("Muon Endpoint for Chi-Square Window of 7 Hits");
+    end_plot->GetXaxis()->SetTitle("Z Position Endpoint from Chi-Square (cm)");
+    end_plot->GetYaxis()->SetTitle("Z Position Endpoint from Truth (cm)");
 
     return true;
   }
@@ -441,8 +443,7 @@ namespace larlite {
     std::cout <<zend <<std::endl;
 
     end_plot -> Fill(z_chi_max, zend);
-    end_plot->GetXaxis()->SetTitle("Z Position Endpoint from Chi-Square (cm)");
-    end-plot->GetYaxis()->SetTitle("Z Position Endpoint from Truth (cm)");
+
      
 
    /*
