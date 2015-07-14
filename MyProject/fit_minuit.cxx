bf7e9e9aa9ff4efa1675b9e7d453b556b73ccb92
Victor Genty <vgenty@nevis.columbia.edu>
2015-07-14 16:43:10 -0500

    made linear fit, chi fucked up

--- MyProject/fit_minuit.cxx
+++ MyProject/fit_minuit.cxx
@@ -21,16 +21,16 @@
 using namespace std;
 
 const int iNum = 5;
-Float_t z[5],x[5],y[5],errorz[5];
+Float_t x[5],y[5],errory[5];
 
 // this is the function used for the fit
 //par: vector with the fit parameters
 
 Double_t fit_function(float x,Double_t *par)
 {
-  //double value=( (par[0]*par[0])/(x*x)-1)/ ( par[1]+par[2]*y-par[3]*y*y);
+  // double value=( (par[0]*par[0])/(x*x)-1)/ ( par[1]+par[2]*y-par[3]*y*y);
   double value=  par[0]+par[1]*x;
- return value;
+  return value;
 }
 
 void calc_chi_square(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag)
@@ -38,7 +38,6 @@ void calc_chi_square(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int
   //calculate chisquare
   double chisq = 0;
   for (int i=0;i<iNum; i++) {
-    // chi square is the quadratic sum of the distance from the point to the function weighted by its error
     double delta  = (y[i]-fit_function(x[i],par))/errory[i];
     chisq += delta*delta;
   }
@@ -48,33 +47,30 @@ void calc_chi_square(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int
 
 int main()
 {
-  // The z values
-  z[0]=1;
-  z[1]=0.96;
-  z[2]=0.89;
-  z[3]=0.85;
-  z[4]=0.78;
+
   // The errors on z values
   Float_t error = 0.01;
-  errorz[0]=error;
-  errorz[1]=error;
-  errorz[2]=error;
-  errorz[3]=error;
-  errorz[4]=error;
-  // the x values
-  x[0]=1.5751;
-  x[1]=1.5825;
-  x[2]=1.6069;
-  x[3]=1.6339;
-  x[4]=1.6706;
-  // the y values
-  y[0]=1.0642;
-  y[1]=0.97685;
-  y[2]=1.13168;
-  y[3]=1.128654;
-  y[4]=1.44016;
+  errory[0]=error;
+  errory[1]=error;
+  errory[2]=error;
+  errory[3]=error;
+  errory[4]=error;
 
-  TMinuit *ptMinuit = new TMinuit(5);  //initialize TMinuit with a maximum of 5 params
+  // the x values
+  x[0]=0.0;
+  x[1]=1.0;
+  x[2]=2.0;
+  x[3]=3.0;
+  x[4]=4.0;
+
+  // the y values
+  y[0]=1.5;
+  y[1]=0.5;
+  y[2]=3.5;
+  y[3]=4.5;
+  y[4]=10.5;
+
+  TMinuit *ptMinuit = new TMinuit(2);  //initialize TMinuit with a maximum of 2 params
   //
   //  select verbose level:
   //    default :     (58 lines in this test)
@@ -85,27 +81,27 @@ int main()
   //     3 : maximum (199 lines in this test)
   //
   ptMinuit->SetPrintLevel();
+
   // set the user function that calculates chi_square (the value to minimize)
   ptMinuit->SetFCN(calc_chi_square);
 
   Double_t arglist[10];
   Int_t ierflg = 0;
-
+  
   arglist[0] = 1;
   ptMinuit->mnexcm("SET ERR", arglist ,1,ierflg);
-
+  
   // Set starting values and step sizes for parameters
-  static Double_t vstart[4] = {3, 1 , 0.1 , 0.01};
-  static Double_t step[4] = {0.1 , 0.1 , 0.01 , 0.001};
-  ptMinuit->mnparm(0, "a1", vstart[0], step[0], 0,0,ierflg);
-  ptMinuit->mnparm(1, "a2", vstart[1], step[1], 0,0,ierflg);
-  ptMinuit->mnparm(2, "a3", vstart[2], step[2], 0,0,ierflg);
-  ptMinuit->mnparm(3, "a4", vstart[3], step[3], 0,0,ierflg);
+  static Double_t vstart[2] = {-5, 1};
+  static Double_t step[2]   = {0.0001 , 0.0001};
+
+  ptMinuit->mnparm(0, "a1", vstart[0], step[0],0,0,ierflg);
+  ptMinuit->mnparm(1, "a2", vstart[1], step[1],0,0,ierflg);
 
   // Now ready for minimization step
   arglist[0] = 500;
   arglist[1] = 1.;
-  ptMinuit->mnexcm("MIGRAD", arglist ,2,ierflg);
+  ptMinuit->mnexcm("MINUIT", arglist,2,ierflg);
 
   // Print results
   cout << "\nPrint results from minuit\n";
@@ -115,10 +111,6 @@ int main()
   cout << "a1=" << fParamVal << "\n";
   ptMinuit->GetParameter(1,fParamVal,fParamErr);
   cout << "a2=" << fParamVal << "\n";
-  ptMinuit->GetParameter(2,fParamVal,fParamErr);
-  cout << "a3=" << fParamVal << "\n";
-  ptMinuit->GetParameter(3,fParamVal,fParamErr);
-  cout << "a4=" << fParamVal << "\n";
 
   // if you want to access to these parameters, use:
   Double_t amin,edm,errdef;
